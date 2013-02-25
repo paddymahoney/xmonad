69784d0b3df6c91b91ee00419eb3f117eacb6a34
Patrick Mahoney <paddy.mahoney@gmail.com>
2013-02-25 17:11:29 -0500

    Modified my layouts. Much nicer.

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,20 @@
 import XMonad
 import XMonad.Config.Kde
+import XMonad.Layout
+import XMonad.Config.Desktop
+import XMonad.Layout.Grid
+import XMonad.Layout.Magnifier
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.NoBorders
+import XMonad.Layout.FixedColumn
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.NoFrillsDecoration
 import qualified XMonad.StackSet as W -- to shift and float windows
  
-main = xmonad kdeConfig
+main = xmonad kde4Config
     { modMask = mod4Mask -- use the Windows button as mod
-    , manageHook = manageHook kdeConfig <+> myManageHook
+    , manageHook = manageHook kde4Config <+> myManageHook
+    , layoutHook = myLayoutHook
     }
  
 myManageHook = composeAll . concat $
@@ -17,3 +27,22 @@ myManageHook = composeAll . concat $
         myOtherFloats = ["alsamixer"]
         webApps       = ["Firefox-bin", "Opera"] -- open on desktop 2
         ircApps       = ["Ksirc"]                -- open on desktop 3
+        
+twoPaneLayout = magnifiercz' 1.4 $ Tall nmaster delta ratio
+    where
+        -- The default number of windows in the master pane
+        nmaster = 1
+        -- Percent of screen to increment by when resizing panes
+        delta   = 3/100
+        -- Default proportion of screen occupied by master pane
+        ratio   = 60/100
+        
+fullWithMagLayout = limitWindows 3 $ magnifiercz' 1.4 $ FixedColumn 1 20 80 10
+
+codeLayout = limitWindows 3 $ FixedColumn 1 20 80 10
+
+deco = noFrillsDeco
+
+myLayoutHook = desktopLayoutModifiers $ deco shrinkText defaultTheme $ smartBorders $ Full ||| fullWithMagLayout ||| twoPaneLayout ||| codeLayout ||| Grid  
+
+        
